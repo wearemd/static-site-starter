bc8a64012fb5c1ef1b13f5cb2c43963ffa2730da
mmaayylliiss <maylis.agniel@gmail.com>
2017-12-20 18:51:22 +0100

    :bug: Prevent gulp-sass watch to not restart after an error

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -1,6 +1,5 @@
 var gulp        = require('gulp');
 var sass        = require('gulp-sass');
-var plumber     = require('gulp-plumber');
 var browserSync = require('browser-sync');
 
 // See https://github.com/sass/node-sass for sass configuration
@@ -12,10 +11,11 @@ var sassConfig = {
   ]
 }
 
+// gulp-sass doesn't work well with gulp-plumber, see: https://github.com/floatdrop/gulp-plumber/issues/32
+// Simple fix by adding .on callback to sass().
 gulp.task('sass', function (done) {
   gulp.src('./sass/style.sass')
-    .pipe(sass(sassConfig))
-    .pipe(plumber())
+    .pipe(sass(sassConfig).on('error', sass.logError))
     .pipe(gulp.dest('./site/css'))
     .on('end', done);
 });
