8df04f578627c101b31d2d2915d6ef0f2d36d8e7
Awea <aweaoftheworld@gmail.com>
2018-01-19 12:26:31 +0100

    :zap: Speed up *.pug compilation

--- gulp/tasks/pug.js
+++ gulp/tasks/pug.js
@@ -2,11 +2,17 @@ var gulp        = require('gulp');
 var pug         = require('gulp-pug');
 var plumber     = require('gulp-plumber');
 var browserSync = require('browser-sync');
+var cache       = require('gulp-cached');
+var progeny     = require('gulp-progeny');
+var filter      = require('gulp-filter');
 
 // See https://pugjs.org/api/reference.html for Pug configuration
 gulp.task('pug', function (done) {
-  gulp.src('./templates/*.pug')
+  gulp.src('./templates/**/*.pug')
     .pipe(plumber())
+    .pipe(cache('pug'))
+    .pipe(progeny())
+    .pipe(filter(['templates/*.pug']))
     .pipe(pug())
     .pipe(gulp.dest('./site'))
     .on('end', done)
