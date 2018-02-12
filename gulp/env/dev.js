9d5db21e345cb97537522b6fdb49a23980dcb8a0
Awea <aweaoftheworld@gmail.com>
2018-02-12 14:49:37 +0100

    :bug: Fix gulp.watch for js/images

--- gulp/env/dev.js
+++ gulp/env/dev.js
@@ -17,11 +17,11 @@ gulp.task('watch', function() {
   gulp.watch(["sass/**/*.sass", "sass/**/*.scss"], gulp.parallel(['sass-reload']));
 
   // If changes happen in site then reload the browser.
-  gulp.watch(["site/fonts/**/*", "site/images/**/*"], browserSync.reload)
+  gulp.watch(["site/fonts/**/*", "site/images/**/*"]).on('change', browserSync.reload)
 
   // JS are compiled using webpack-stream with option watch set to true for performances reason.
   // So the script task never ends and we need to watch manually for changes on generated JS.
-  gulp.watch(["site/js/app.js"], browserSync.reload)
+  gulp.watch(["site/js/*.js"]).on('change', browserSync.reload)
 })
 
 gulp.task('default', gulp.parallel(['browser-sync', 'watch', 'sass', 'pug', 'script']));
