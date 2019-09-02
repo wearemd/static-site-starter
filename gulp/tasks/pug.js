c375ece1ed108ce825a58fa95f8712ab8c033191
Awea <aweaoftheworld@gmail.com>
2019-09-02 09:54:36 +0200

    :bug: Remove buggy pug cache

--- gulp/tasks/pug.js
+++ gulp/tasks/pug.js
@@ -1,18 +1,12 @@
 var gulp = require("gulp");
 var pug = require("gulp-pug");
 var browserSync = require("browser-sync");
-var cache = require("gulp-cached");
-var progeny = require("gulp-progeny");
-var filter = require("gulp-filter");
 var { assetVersion } = require("../helpers/url");
 
 // See https://pugjs.org/api/reference.html for Pug configuration
 gulp.task("pug", function() {
   return gulp
-    .src("./templates/**/*.pug")
-    .pipe(cache("pug"))
-    .pipe(progeny())
-    .pipe(filter(["templates/*.pug"]))
+    .src("./templates/*.pug")
     .pipe(
       pug({
         data: {
